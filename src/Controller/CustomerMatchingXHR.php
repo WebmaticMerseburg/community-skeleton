23028d740657c8a09e836788fb8b69544f808135
Maximilian Hofmann <mh@webmatic.de>
2022-03-09 17:03:30 +0100

    domain auto assignment

--- src/Controller/CustomerMatchingXHR.php
+++ src/Controller/CustomerMatchingXHR.php
@@ -17,6 +17,107 @@ use Webmatic\KundeBundle\Entity\Kunde;
 
 class CustomerMatchingXHR extends AbstractController {
 
+    const SKIP_DOMAINS = [
+        "googlemail.com", "gmail.com", "yahoo.com", "yahoo.de", "aol.com", "aol.de",
+        "live.com", "live.de", "hotmail.com", "hotmail.de", "outlook.com", "outlook.de",
+        "msn.com", "msn.de", "gmx.de", "gmx.net", "web.de", "mail.ru", "freenet.de", "t-online.de",
+        "mail.com", "mail.de", "icloud.com"
+    ];
+
+    /**
+     * @Route(
+     *  "/%uvdesk_site_path.member_prefix%/wmt/skip-domains/list",
+     *  name="webmatic_skip_domains_list"),
+     *  methods={"HEAD", "GET"})
+     */
+    function getSkipDomains() : Response {
+        return $this->json($this::SKIP_DOMAINS);
+    }
+
+    /**
+     * @Route(
+     *  "/%uvdesk_site_path.member_prefix%/wmt/check/auto-assign",
+     *  name="webmatic_check_auto_assign_domain"),
+     *  methods={"HEAD", "GET"})
+     */
+    function checkAutoDomainAssignment(ORMEntityManagerInterface $em) : Response {
+        
+        $kunden = $em->getRepository(Kunde::class)
+                    ->findAll();
+
+        $domains = [];
+        foreach ($kunden as $k) {
+            if ($email = $k->getEmail()) {
+                $domain = substr($email, strpos($email, "@") + 1);
+                if (!in_array($domain, $this::SKIP_DOMAINS)) {
+                    $domains[] = $domain;
+                }
+            }
+        }
+
+        $uniqueDomains = array_unique($domains);
+        $multipleDomains = [];
+        foreach ($uniqueDomains as $uniqueDomain) {
+            $v = array_keys($domains, $uniqueDomain);
+        
+            if (count($v) > 1) {
+                $multipleDomains[] = $uniqueDomain;        
+            }
+        }
+        
+        if (count($multipleDomains) > 0) {
+            $domainResponse = implode(", ", $multipleDomains);
+            return $this->json(["alertClass" => "warning", "alertMessage" => "Doppelte Domains: " . $domainResponse . ". Korrektur notwendig, um Zuordnung fehlerfrei durchführen zu können."], 400);
+        } else {
+            return new Response();
+        }
+        
+
+    }
+
+    /**
+     * @Route(
+     *  "/{_locale}/%uvdesk_site_path.member_prefix%/wmt/customer/auto-assign",
+     *  name="webmatic_customer_auto_assign_domain"),
+     *  methods={"HEAD", "PUT"})
+     */
+    function autoDomainAssignment(ORMEntityManagerInterface $em) : Response {
+
+        try {
+
+            $kunden = $em->getRepository(Kunde::class)
+                        ->findAll();
+
+            $entities = [];
+            foreach ($kunden as $k) {
+                if ($email = $k->getEmail()) {
+                    $domain = substr($email, strpos($email, "@") + 1);
+                    if (!in_array(strtolower($domain), $this::SKIP_DOMAINS)) {
+                        $entity = new KundeDomain();
+                        if ($persisted = $em->getRepository(KundeDomain::class)
+                                            ->findOneByKunde($k)
+                        ) {
+                            $entity = $persisted;
+                        }
+                        $entity->setDomain($domain)->setKunde($k);
+                        $entities[] = $entity;
+                    }
+                }
+            } 
+            
+            $em->saveEntities($entities);
+
+            $msg = "Die Domains der Kontaktadressen wurden automatisch den Kunden zugewiesen.";
+            $this->addFlash("success", $msg);
+            return $this->json(["alertClass" => "success", "alertMessage" => $msg]);
+        } catch( Exception $e) {
+            $msg = "Beim Zuweisen der Domains ist ein Fehler aufgetreten: ".$e->getMessage();
+            $this->addFlash("warning", $msg);
+            return $this->json(["error" => $msg], 400);
+        }    
+
+    }
+
     /**
      * @Route(
      *  "/{_locale}/%uvdesk_site_path.member_prefix%/wmt/customer/match/{customer}/{domain}/set",
