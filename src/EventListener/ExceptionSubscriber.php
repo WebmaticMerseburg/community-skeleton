201715a1e69c7901175aff701a129830dd7ae038
Maximilian Hofmann <mh@webmatic.de>
2022-03-25 14:02:47 +0100

    fix deprecated getException

--- src/EventListener/ExceptionSubscriber.php
+++ src/EventListener/ExceptionSubscriber.php
@@ -41,7 +41,7 @@ class ExceptionSubscriber implements EventSubscriberInterface
 		}
 
 		// @TODO: We need to take into account the response type as well (html, xml, json)
-		$exception = $event->getException();
+		$exception = $event->getThrowable();
 
 		if ($exception->getCode() == 403) {
 			// On forbidden exception, we need to either:
